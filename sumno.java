9fab84ca7480baab83194af285fca59464525a27
MOHD_SHAHID <146314603+Shahid-cs22@users.noreply.github.com>
2023-11-10 08:38:11 +0530

    Add files via upload

---
+++ sumno.java
@@ -0,0 +1,14 @@
+class sumno{
+    public static void main(String args[]){
+        int sum=0;
+        for(int i=1;i<=100;i++){
+            if(i%2!=0){
+                sum=sum+i;
+            }
+
+        }
+        System.out.println("The sum of the number are:"+sum);
+        
+    }
+    
+}
